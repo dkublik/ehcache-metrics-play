cae992b6fd3dbe4b78c49449c0e9ba3f16bff9bf
Stephane Nicoll <snicoll@pivotal.io>
2019-07-19 18:16:17 +0200

    Polish

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ repositories {
 }
 
 configure<JavaPluginConvention> {
-	sourceCompatibility = JavaVersion.VERSION_11
-	targetCompatibility = JavaVersion.VERSION_11
+	sourceCompatibility = JavaVersion.VERSION_1_8
+	targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 dependencies {
--- src/main/java/pl/dk/learn/ehmetrics/CacheConfig.java
+++ src/main/java/pl/dk/learn/ehmetrics/CacheConfig.java
@@ -2,6 +2,7 @@ package pl.dk.learn.ehmetrics;
 
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.config.CacheConfiguration;
+import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.context.annotation.Bean;
@@ -16,13 +17,13 @@ class CacheConfig {
     static final int MAX_ELEMENTS_IN_MEMORY = 200;
 
     @Bean
-    Cache playCache(EhCacheCacheManager cacheManager) {
-        CacheConfiguration cacheConfiguration = new CacheConfiguration()
-            .name(CACHE_NAME)
-            .maxEntriesLocalHeap(MAX_ELEMENTS_IN_MEMORY);
-        Cache cache = new Cache(cacheConfiguration);
-        cacheManager.getCacheManager().addCache(cache);
-        cacheManager.initializeCaches();
-        return cache;
+    CacheManagerCustomizer<EhCacheCacheManager> playCacheCustomizer() {
+        return cacheManager -> {
+            CacheConfiguration cacheConfiguration = new CacheConfiguration()
+                    .name(CACHE_NAME)
+                    .maxEntriesLocalHeap(MAX_ELEMENTS_IN_MEMORY);
+            Cache cache = new Cache(cacheConfiguration);
+            cacheManager.getCacheManager().addCache(cache);
+        };
     }
 }
